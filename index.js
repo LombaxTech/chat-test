5189eac4879d14997c048d800c6d4edab87de1c0
Rakib Khan <rakibkhan@live.co.uk>
2019-07-03 16:05:52 +0100

    added procfile

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var PORT = process.env.PORT || 5000;
 
 app.get('/',(req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -12,6 +13,6 @@ io.on('connection', function(socket){
     });
   });
 
-http.listen(3000, ()=>{
-    console.log('listening on port 3000')
+http.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`)
 })
